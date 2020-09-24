43fc81b3d345283b491be572bc0441390c608666
Jimmy Svensson <jimmy888swe@gmail.com>
2020-09-24 22:14:56 +0200

    fix: diffent accordion

--- src/App.js
+++ src/App.js
@@ -5,7 +5,18 @@ import Accordion from './components/Accordion';
 function App() {
   return (
     <div className="App">
-    <Accordion />
+    <Accordion 
+      title='npx create-react-app'
+      body='It sets up your development environment so that you can use the latest JavaScript features, provides a nice developer experience, and optimizes your app for production.'
+       />
+    <Accordion 
+      title='Ut rutrum sagittis libero' 
+      body='Aenean id risus quis dolor convallis viverra. Suspendisse ornare ex et rutrum fringilla. Maecenas interdum vel turpis vel egestas. Phasellus viverra justo non risus faucibus maximus.'
+       />
+    <Accordion 
+      title='Lorem ipsum'
+      body='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel augue mauris. Donec lacinia hendrerit lacus a congue. Proin commodo nec diam quis dapibus.'
+      />
     </div>
   );
 }
