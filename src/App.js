d5186e457c1de9e5ab22c77197d949a8d3d461ec
Jimmy Svensson <jimmy888swe@gmail.com>
2020-09-24 21:57:16 +0200

    feat: move to wn components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
+import Accordion from './components/Accordion';
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="App">
-     <div className="accordion">
-     <div className="accordion-header"><h2>npx create-react-app</h2></div>
-    {isOpen && (
-      <div className="accordion-body"><p>It sets up your development environment so that you can use the latest JavaScript features, provides a nice developer experience, and optimizes your app for production</p></div>
-    )}
-     </div>
+    <Accordion />
     </div>
   );
 }
