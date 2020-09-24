b2aad1137492950f15aaf55df5aaff82d5a6758d
Jimmy Svensson <jimmy888swe@gmail.com>
2020-09-24 22:15:32 +0200

    fix: pass value from parent

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,16 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
-function Accordion() {
+function Accordion({title, body}) {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div>
     <div className="App">
     <div className="accordion">
-    <div onClick={()=> {setIsOpen(!isOpen)}} className="accordion-header"><h2>npx create-react-app</h2><span className="inducator">{isOpen ? '-' : '+'}</span></div>
+    <div onClick={()=> {setIsOpen(!isOpen)}} className="accordion-header"><h2>{title}</h2><span className="inducator">{isOpen ? '-' : '+'}</span></div>
 
    {isOpen && (
-     <div className="accordion-body"><p>It sets up your development environment so that you can use the latest JavaScript features, provides a nice developer experience, and optimizes your app for production</p></div>
+     <div className="accordion-body"><p>{body}</p></div>
    )}
     </div>
    </div>
