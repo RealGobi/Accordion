54a9a481d84ca1f066d9e2d21e5a448a3c725088
Jimmy Svensson <jimmy888swe@gmail.com>
2020-09-24 21:37:09 +0200

    state: isOpen

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
     <div className="App">
      <div className="accordion">
      <div className="accordion-header"><h2>npx create-react-app</h2></div>
-     <div className="accordion-body"><p>It sets up your development environment so that you can use the latest JavaScript features, provides a nice developer experience, and optimizes your app for production</p></div>
+    {isOpen && (
+      <div className="accordion-body"><p>It sets up your development environment so that you can use the latest JavaScript features, provides a nice developer experience, and optimizes your app for production</p></div>
+    )}
      </div>
     </div>
   );
